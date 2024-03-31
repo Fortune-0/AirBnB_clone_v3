02b49eafa55812a6eee66c8a160c94cda22aff74
Ano-ly <amarachiuvere@student.oauife.edu.ng>
2024-03-31 10:13:11 +0100

    Fix errors for task 3

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 """New file, purpose yet to be found"""
 
-from flask import jsonify
 from api.v1.views import app_views
+from flask import jsonify
+import json
 
 
 @app_views.route("/status")
 def return_json():
     """Return message in json format"""
-    return jsonify({"status": "OK"})
+    dict = {"status": "OK"}
+    return (jsonify(dict))
