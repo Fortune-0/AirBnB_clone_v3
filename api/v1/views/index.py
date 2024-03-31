4a3dc93dcc9e768265ffc7f7e5a9991ebd02999d
Ano-ly <amarachiuvere@student.oauife.edu.ng>
2024-03-31 09:24:09 +0100

    Complete task 3

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 """New file, purpose yet to be found"""
 
+from flask import jsonify
 from api.v1.views import app_views
-from Flask import jsonify
 
 
 @app_views.route("/status")
 def return_json():
     """Return message in json format"""
-    dic_to_json = {"status": "OK"}
-    return (jsonify.dic_to_json)
+    return {
+            "status": "OK"
+    }
