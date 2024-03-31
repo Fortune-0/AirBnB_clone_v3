9d969a7d72270fe85db511b6bcb62f3e2ac62764
Ano-ly <amarachiuvere@student.oauife.edu.ng>
2024-03-31 12:15:13 +0100

    Complete task 5

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """Define application as instance of Flask"""
 from api.v1.views import app_views
-from flask import Flask
+from flask import Flask, jsonify, make_response
 from models import storage
 from os import environ
 
@@ -13,6 +13,13 @@ app.register_blueprint(app_views, url_prefix='/api/v1')
 def teardown(exception=None):
     """Destroy current session at the end of request"""
     storage.close()
+
+
+@app.errorhandler(404)
+def error_handler(err):
+    """Handle the 404 error by returning JSON response"""
+    error_dict = {"error": "Not found"}
+    return make_response(jsonify(error_dict), 404)
 
 
 if __name__ == "__main__":
