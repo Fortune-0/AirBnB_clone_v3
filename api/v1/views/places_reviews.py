abf74210b6e1abb84cbe0e24b542fb1aeb7b5786
Ano-ly <amarachiuvere@student.oauife.edu.ng>
2024-04-02 05:38:20 +0100

    Resolve errors dealing with absent user_id in database

--- api/v1/views/places_reviews.py
+++ api/v1/views/places_reviews.py
@@ -76,6 +76,11 @@ def create_review(place_id):
     if 'user_id' not in _instance.keys():
         abort(400, "Missing user_id")
     user_dict = storage.all(User)
+    for item in user_dict.values():
+        if user.id == _instance['user_id]:
+            break
+    else:
+        abort(404)
     if 'text' not in _instance.keys():
         abort(400, "Missing text")
     new_review = Review(**_instance)
